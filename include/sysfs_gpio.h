68c88a961be8ac29410ccf211f330c13ea255e51
Amirhossein Hajlou - Raspberry Pi <hajlouamirhossein@gmail.com>
2023-05-30 10:11:08 +0100

    add sysfs_gpio

---
+++ include/sysfs_gpio.h
@@ -0,0 +1,23 @@
+#ifndef SYSFS_GPIO_H
+#define SYSFS_GPIO_H
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define IN  0
+#define OUT 1
+
+#define LOW  0
+#define HIGH 1
+
+static int GPIOExport(int pin);
+static int GPIOUnexport(int pin);
+static int GPIODirection(int pin, int dir);
+static int GPIORead(int pin);
+static int GPIOWrite(int pin, int value);
+
+#endif
