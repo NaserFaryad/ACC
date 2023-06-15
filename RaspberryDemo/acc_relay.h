ddb126feb3b23165aedc79340ddc046ecfc6aa19
naserfaryad <naserfaryad@gmail.com>
2023-06-15 23:10:33 +0330

    tested panel LEDs success

---
+++ RaspberryDemo/acc_relay.h
@@ -0,0 +1,4 @@
+#ifndef ACC_RELAY_H
+#define ACC_RELAY_H
+
+#endif // ACC_RELAY_H
--- RaspberryDemo/file_gpio.h
+++ RaspberryDemo/file_gpio.h
@@ -10,6 +10,7 @@ extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "bcm2835.h"
 
 #define IN  0
 #define OUT 1
@@ -20,6 +21,13 @@ extern "C" {
 #define PIN  24 /* P1-18 */
 #define POUT 4  /* P1-07 */
 
+/* Relay GPIO*/
+#define WARN_LED_PIN    15
+#define SENSOR_OK_PIN   23
+#define PWR_5V_OK_PIN   4
+#define PWR_15V_OK_PIN  3
+#define SQR_SIN_SEL_PIN 6
+#define PWR_15V_SW_PIN  14
 
 int GPIOExport(int pin);
 
@@ -28,6 +36,18 @@ int GPIOUnexport(int pin);
 int GPIODirection(int pin, int dir);
 int GPIORead(int pin);
 int GPIOWrite(int pin, int value);
+
+int relay_init();
+void sensor_ok_on();
+void sensor_ok_off();
+void pwr_15v_ok_on();
+void pwr_15v_ok_off();
+void toggle_warning(int ms_delay);
+void pwr_5v_ok_on();
+void pwr_5v_ok_off();
+void pwr_15v_on();
+void pwr_15v_off();
+void relays_off();
 
 
 #ifdef __cplusplus
